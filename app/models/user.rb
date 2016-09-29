0d9584f10cbb4301a2a5a4a3e8d40470fa511554
santiago <smartnix@hotmail.com>
2016-09-29 18:46:33 -0500

    Se arreglo la vista de los articulos, falla en ajax con los comentarios, se agrego avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   has_many :articulos
   has_many :comments
   include PermissionsConcern
+
+  def avatar
+  	# get the email from URL-parameters or what have you and make lowercase
+	email_address = self.email.downcase
+	 
+	# create the md5 hash
+	hash = Digest::MD5.hexdigest(email_address)
+	 
+	# compile URL which can be used in <img src="RIGHT_HERE"...
+	image_src = "https://www.gravatar.com/avatar/#{hash}"
+  	
+  end
 end
